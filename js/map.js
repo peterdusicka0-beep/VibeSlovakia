9b4e03109ca74007abd1d527406dbd8c28c75157
peterdusicka0-beep <peterdusicka0@gmail.com>
2026-02-02 19:06:41 +0100

    Pridavam mapu a hrady

--- js/map.js
+++ js/map.js
@@ -1,14 +1,10 @@
-console.log("Načítané hrady:", castles.length);
-
 const map = L.map("map").setView([48.7, 19.5], 7);
-
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap"
 }).addTo(map);
 
 castles.forEach(castle => {
-  if(typeof castle.lat !== "number" || typeof castle.lng !== "number") return;
   L.marker([castle.lat, castle.lng])
     .addTo(map)
-    .bindPopup(`<strong>${castle.name}</strong><br>${castle.desc}`);
+    .bindPopup(`<b>${castle.name}</b><br>${castle.desc}`);
 });
