2d236b97d15e6774a975f84f212fa557575a37a9
peterdusicka0-beep <peterdusicka0@gmail.com>
2026-02-02 16:26:11 +0100

    Oprava logiky mapy a prepojenia na castles.js
    - Inicializácia Leaflet mapy so stredom na Slovensko.
    - Pridanie OpenStreetMap vrstvy.
    - Implementácia cyklu na automatické vykreslenie hradov z castles.js.

--- js/map.js
+++ js/map.js
@@ -1,16 +1,20 @@
+// Skontrolujeme, či sú dáta načítané
 if (typeof castles === "undefined") {
-  alert("CHYBA: Súbor castles.js sa nenačítal");
+  console.error("CHYBA: Súbor castles.js sa nenačítal!");
+} else {
+  // Inicializácia mapy
+  const map = L.map("map").setView([48.7, 19.5], 7);
+
+  // Pridanie mapových podkladov
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap"
+  }).addTo(map);
+
+  // Vykreslenie hradov
+  castles.forEach(castle => {
+    // POZOR: Tu používame castle.lat a castle.lng presne podľa tvojho castles.js
+    L.marker([castle.lat, castle.lng])
+      .addTo(map)
+      .bindPopup(`<b>${castle.name}</b><br>${castle.desc}`);
+  });
 }
-
-const map = L.map("map").setView([48.7, 19.5], 7);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap"
-}).addTo(map);
-
-castles.forEach(castle => {
-  L.marker([castle.lat, castle.lng])
-    .addTo(map)
-    .bindPopup(`<strong>${castle.name}</strong><br>${castle.desc}`);
-});
-
