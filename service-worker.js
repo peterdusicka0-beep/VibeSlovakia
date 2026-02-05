e55995e12d388cf1b4906e5ee09d736e63987495
peterdusicka0-beep <peterdusicka0@gmail.com>
2026-02-06 00:03:39 +0100

    Enable offline mode using service worker (PWA)

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = "vibeslovakia-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+
+  "./data/castles.js",
+  "./js/map.js",
+
+  "./assets/audio/placeholder.mp3",
+
+  "./assets/images/bratislava_ai.webp",
+  "./assets/images/spis_ai.webp",
+  "./assets/images/orava_ai.webp",
+  "./assets/images/trencin_ai.webp",
+  "./assets/images/bojnice_ai.webp",
+
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
